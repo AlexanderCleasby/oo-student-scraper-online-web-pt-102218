8b5e48edb2e5d3ff48377d33c77a2f29d53dacf2
alexandercleasby <cleasby.alex@gmail.com>
2018-12-14 17:24:00 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,40 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    #puts doc.css('.student-card')[0]
+    doc.css('.student-card a').map{|card|
+    {:name => card.css('.student-name').text,
+      :location=> card.css('.student-location').text,
+      :profile_url=>"#{card.attr('href')}"
+      }}
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    #binding.pry
+    out = {
+      #:twitter=>doc.css('.social-icon-container a').attr('href'),
+      #:linkedin=>doc.css('.social-icon-container a')[1].attr('href'),
+      #:github=>doc.css('.social-icon-container a')[2].attr('href'),
+      #:blog=>doc.css('.social-icon-container a')[3].attr('href'),
+      :profile_quote=>doc.css('.profile-quote').text,
+      :bio=>doc.css('.description-holder p').text
+    }
+    doc.css('.social-icon-container a').map{|e| e.attr('href')}.each{|href|
+      if href.include?('twitter')
+        out[:twitter] = href
+      elsif href.include?('linkedin')
+        out[:linkedin]=href
+      elsif href.include?('github')
+        out[:github]=href
+      else
+        out[:blog]=href
+      end
+    }
+    out
   end
+
 
 end
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,29 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    #@profile_quote = student_hash.profile_quote
+    #@profile_url
+    #@bio = student_hash.bio
+    #@twitter
+    #@linkedin
+    #@github
+    #@blog
+    @@all.push(self)
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each{|student|self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each{|k,v| 
+    self.send(k.to_s+'=',v)}
   end
 
   def self.all
-    
+    @@all
   end
 end
 
